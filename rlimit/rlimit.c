7332e2d5f63abd8c3416cfbc50af51fba1c38d11
Dimitri Sokolyuk <demon@dim13.org>
2010-01-31 08:20:10 +0000

    update email

--- rlimit/rlimit.c
+++ rlimit/rlimit.c
@@ -1,6 +1,6 @@
 /* $Id$ */
 /*
- * Copyright (c) 2005 Dimitri Sokolyuk <demon@vhost.dyndns.org>
+ * Copyright (c) 2005 Dimitri Sokolyuk <demon@dim13.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
